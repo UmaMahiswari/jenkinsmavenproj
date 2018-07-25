3215295008c76fbb5d82571277c0fc732dbbe534
IBM <IBM@DEVOPS_01>
2018-07-25 14:42:22 +0530

    code

---
+++ jenkinsmavenartifact/src/test/java/jenkinsmavenartifact/Test_Runner.java
@@ -0,0 +1,10 @@
+package jenkinsmavenartifact;
+
+public class Test_Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("it is working");
+	}
+
+}
